658c00c1da9525d50f6cb179b2671b1407ac48a7
lotoanalyzer <131580150+lotoanalyzer@users.noreply.github.com>
2023-04-23 04:56:10 -0300

    ok
    ok

---
+++ script.js
@@ -0,0 +1,11 @@
+<script>
+  function showPopup(popupId) {
+    var popup = document.getElementById(popupId);
+    popup.style.display = "block";
+  }
+
+  function hidePopup(popupId) {
+    var popup = document.getElementById(popupId);
+    popup.style.display = "none";
+  }
+</script>
